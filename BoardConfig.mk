aaa09250a0ffcd9a86cfc344743897ef7622266a
Isaac Joseph <ikarosdev@gmail.com>
2011-07-27 18:08:43 -0500

    Start adding applicable information

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -4,13 +4,14 @@ USE_CAMERA_STUB := true
 -include vendor/motorola/triumph/BoardConfigVendor.mk
 
 TARGET_NO_BOOTLOADER := true
-TARGET_BOARD_PLATFORM := unknown
-TARGET_CPU_ABI := armeabi
+TARGET_BOARD_PLATFORM := msm7x30
+TARGET_CPU_ABI := armeabi-v7a
+TARGET_CPU_ABI2 := armeabi
 TARGET_BOOTLOADER_BOARD_NAME := triumph
 
 BOARD_KERNEL_CMDLINE := console=ttyMSM1 androidboot.hardware=qcom
 BOARD_KERNEL_BASE := 0x00200000
-BOARD_PAGE_SIZE := 0x00001000
+BOARD_PAGE_SIZE := 4096
 
 # fix this up by examining /proc/mtd on a running device
 BOARD_BOOTIMAGE_PARTITION_SIZE := 0x00380000
@@ -20,6 +21,20 @@ BOARD_USERDATAIMAGE_PARTITION_SIZE := 0x105c0000
 BOARD_FLASH_BLOCK_SIZE := 131072
 
 TARGET_PREBUILT_KERNEL := device/motorola/triumph/kernel
+
+# Filesystem
+BOARD_DATA_DEVICE := /dev/block/mmcblk0p8
+BOARD_DATA_FILESYSTEM := ext3
+BOARD_CACHE_DEVICE := /dev/block/mmcblk0p11
+BOARD_CACHE_FILESYSTEM := ext3
+#BOARD_SDCARD_DEVICE_PRIMARY := /dev/block/mmcblk1p1
+BOARD_SYSTEM_FILESYSTEM := ext3
+BOARD_SYSTEM_DEVICE := /dev/block/mmcblk0p7
+#BOARD_HAS_NO_MISC_PARTITION := true
+
+# Recovery
+BOARD_USES_MMCUTILS := true
+BOARD_CUSTOM_RECOVERY_KEYMAPPING:= ../../device/motorola/triumph/recovery/recovery_ui.c
 
 #BOARD_HAS_NO_SELECT_BUTTON := true
 # Use this flag if the board has a ext4 partition larger than 2gb
