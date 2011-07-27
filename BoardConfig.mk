ca63dc3b9f805aeeb536d512006bae8914d99eb0
Isaac Joseph <ikarosdev@gmail.com>
2011-07-27 17:53:09 -0500

    add new Triumph configs

---
+++ BoardConfig.mk
@@ -0,0 +1,26 @@
+USE_CAMERA_STUB := true
+
+# inherit from the proprietary version
+-include vendor/motorola/triumph/BoardConfigVendor.mk
+
+TARGET_NO_BOOTLOADER := true
+TARGET_BOARD_PLATFORM := unknown
+TARGET_CPU_ABI := armeabi
+TARGET_BOOTLOADER_BOARD_NAME := triumph
+
+BOARD_KERNEL_CMDLINE := console=ttyMSM1 androidboot.hardware=qcom
+BOARD_KERNEL_BASE := 0x00200000
+BOARD_PAGE_SIZE := 0x00001000
+
+# fix this up by examining /proc/mtd on a running device
+BOARD_BOOTIMAGE_PARTITION_SIZE := 0x00380000
+BOARD_RECOVERYIMAGE_PARTITION_SIZE := 0x00480000
+BOARD_SYSTEMIMAGE_PARTITION_SIZE := 0x08c60000
+BOARD_USERDATAIMAGE_PARTITION_SIZE := 0x105c0000
+BOARD_FLASH_BLOCK_SIZE := 131072
+
+TARGET_PREBUILT_KERNEL := device/motorola/triumph/kernel
+
+#BOARD_HAS_NO_SELECT_BUTTON := true
+# Use this flag if the board has a ext4 partition larger than 2gb
+#BOARD_HAS_LARGE_FILESYSTEM := true
