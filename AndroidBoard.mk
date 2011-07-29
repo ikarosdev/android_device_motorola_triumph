cef82b54c5a365565397544538f29a94be3dae29
Isaac Joseph <ikarosdev@gmail.com>
2011-07-29 00:38:18 -0500

    attempt to emulate the original recovery

--- AndroidBoard.mk
+++ AndroidBoard.mk
@@ -4,5 +4,35 @@ include $(CLEAR_VARS)
 
 ALL_PREBUILT += $(INSTALLED_KERNEL_TARGET)
 
+file := $(TARGET_RECOVERY_ROOT_OUT)/default.prop
+ALL_PREBUILT += $(file)
+$(file) : $(LOCAL_PATH)/recovery/default.prop | $(ACP)
+	$(transform-prebuilt-to-target)
+
+file := $(TARGET_RECOVERY_ROOT_OUT)/sbin/fih_imgupd
+ALL_PREBUILT += $(file)
+$(file) : $(LOCAL_PATH)/recovery/sbin/fih_imgupd | $(ACP)
+	$(transform-prebuilt-to-target)
+
+file := $(TARGET_RECOVERY_ROOT_OUT)/system/etc/dfi/FIH_FB0.dfi
+ALL_PREBUILT += $(file)
+$(file) : $(LOCAL_PATH)/recovery/system/etc/dfi/FIH_FB0.dfi | $(ACP)
+	$(transform-prebuilt-to-target)
+
+file := $(TARGET_RECOVERY_ROOT_OUT)/system/etc/dfi/FIH_SF5.dfi
+ALL_PREBUILT += $(file)
+$(file) : $(LOCAL_PATH)/recovery/system/etc/dfi/FIH_SF5.dfi | $(ACP)
+	$(transform-prebuilt-to-target)
+
+file := $(TARGET_RECOVERY_ROOT_OUT)/system/etc/dfi/FIH_SF6.dfi
+ALL_PREBUILT += $(file)
+$(file) : $(LOCAL_PATH)/recovery/system/etc/dfi/FIH_SF6.dfi | $(ACP)
+	$(transform-prebuilt-to-target)
+
+file := $(TARGET_RECOVERY_ROOT_OUT)/system/etc/dfi/FIH_SF8.dfi
+ALL_PREBUILT += $(file)
+$(file) : $(LOCAL_PATH)/recovery/system/etc/dfi/FIH_SF8.dfi | $(ACP)
+	$(transform-prebuilt-to-target)
+
 # include the non-open-source counterpart to this file
 -include vendor/motorola/triumph/AndroidBoardVendor.mk
--- BoardConfig.mk
+++ BoardConfig.mk
@@ -3,19 +3,19 @@ USE_CAMERA_STUB := true
 # inherit from the proprietary version
 -include vendor/motorola/triumph/BoardConfigVendor.mk
 
-TARGET_NO_BOOTLOADER := true
-TARGET_BOARD_PLATFORM := msm7x30
+TARGET_NO_BOOTLOADER := false
+#TARGET_BOARD_PLATFORM := msm7x30
+
 TARGET_CPU_ABI := armeabi-v7a
 TARGET_CPU_ABI2 := armeabi
 TARGET_BOOTLOADER_BOARD_NAME := triumph
 
 BOARD_KERNEL_CMDLINE := console=ttyMSM1 androidboot.hardware=qcom
-BOARD_KERNEL_BASE := 0x00200000
+BOARD_KERNEL_BASE := 0x20000000
 BOARD_PAGE_SIZE := 4096
 
-# fix this up by examining /proc/mtd on a running device
-BOARD_BOOTIMAGE_PARTITION_SIZE := 0x00380000
-BOARD_RECOVERYIMAGE_PARTITION_SIZE := 0x00480000
+BOARD_BOOTIMAGE_PARTITION_SIZE := 0x00500000
+BOARD_RECOVERYIMAGE_PARTITION_SIZE := 0x00500000
 BOARD_SYSTEMIMAGE_PARTITION_SIZE := 0x08c60000
 BOARD_USERDATAIMAGE_PARTITION_SIZE := 0x105c0000
 BOARD_FLASH_BLOCK_SIZE := 131072
@@ -36,8 +36,7 @@ BOARD_SDEXT_DEVICE := /dev/block/mmcblk1p2
 
 # Recovery
 BOARD_USES_MMCUTILS := true
-BOARD_CUSTOM_RECOVERY_KEYMAPPING:= ../../device/motorola/triumph/recovery/recovery_ui.c
+TARGET_RECOVERY_INITRC := device/motorola/triumph/recovery.rc
+BOARD_CUSTOM_RECOVERY_KEYMAPPING:= ../../device/motorola/triumph/recovery_ui.c
 
-#BOARD_HAS_NO_SELECT_BUTTON := true
-# Use this flag if the board has a ext4 partition larger than 2gb
-#BOARD_HAS_LARGE_FILESYSTEM := true
+BOARD_HAS_NO_SELECT_BUTTON := true
