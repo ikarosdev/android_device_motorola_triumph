755d97c7198ff9f9309752ae565233d1f33e94bd
Isaac Joseph <ikarosdev@gmail.com>
2011-07-30 12:54:13 -0500

    start adding files from original system

--- triumph.mk
+++ triumph.mk
@@ -7,6 +7,30 @@ $(call inherit-product-if-exists, vendor/motorola/triumph/triumph-vendor.mk)
 
 DEVICE_PACKAGE_OVERLAYS += device/motorola/triumph/overlay
 
+# Init files
+PRODUCT_COPY_FILES += \
+    device/motorola/triumph/ueventd.qcom.rc:root/ueventd.qcom.rc
+
+# Properties taken from default.prop
+PRODUCT_PROPERTY_OVERRIDES += \
+    ro.com.google.clientidbase=android-motorola \
+    ro.com.google.clientidbase.yt=android-motorola \
+    ro.com.google.clientidbase.am=android-motorola \
+    ro.com.google.clientidbase.ms=android-motorola \
+    ro.com.google.clientidbase.gmm=android-motorola \
+    ro.cdma.home.operator.alpha=Virgin Mobile \
+    gsm.operator.alpha=Virgin Mobile \
+    ro.telephony.gsm.ecclist=110,112,119,911 \
+    ro.telephony.cdma.ecclist=110,112,119,911 \
+    ro.telephony.ecclist=08,000,110,112,118,119,911,999 \
+    ro.telephony.num.auto.hyphen=true \
+    ro.telephony.gsm.spn.shortname=true \
+
+# Permissions
+PRODUCT_COPY_FILES += \
+    frameworks/base/data/etc/android.hardware.telephony.cdma.xml:system/etc/permissions/android.hardware.telephony.cdma.xml \
+    frameworks/base/data/etc/android.hardware.sensor.accelerometer.xml:system/etc/permissions/android.hardware.sensor.accelerometer.xml \
+    frameworks/base/data/etc/android.hardware.sensor.compass.xml:system/etc/permissions/android.hardware.sensor.compass.xml
 
 ifeq ($(TARGET_PREBUILT_KERNEL),)
 	LOCAL_KERNEL := device/motorola/triumph/kernel
