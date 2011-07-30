c3d4beedb0b430549f7dd73f2bf8170b91525f50
Isaac Joseph <ikarosdev@gmail.com>
2011-07-29 23:54:31 -0500

    clean out unused items, fix recovery.fstab (thanks to tystyle)

--- recovery/recovery_ui.c
+++ recovery/recovery_ui.c
@@ -20,7 +20,9 @@
 #include "common.h"
 #include "extendedcommands.h"
 
-char* MENU_HEADERS[] = { NULL };
+char* MENU_HEADERS[] = { "Use vol keys to highlight and menu to select.",
+                         "",
+                         NULL };
 
 char* MENU_ITEMS[] = { "reboot system now",
                        "apply update from sdcard",
@@ -56,16 +58,12 @@ int device_reboot_now(volatile char* key_pressed, int key_code) {
 int device_handle_key(int key_code, int visible) {
     if (visible) {
         switch (key_code) {
-            case KEY_CAPSLOCK:
-            case KEY_DOWN:
-            case KEY_VOLUMEDOWN:
-            case KEY_MENU:
+            //case KEY_VOLUMEDOWN:
+	    case 115:
                 return HIGHLIGHT_DOWN;
 
-            case KEY_LEFTSHIFT:
-            case KEY_UP:
-            case KEY_VOLUMEUP:
-            case KEY_HOME:
+            //case KEY_VOLUMEUP:
+	    case 114:
                 return HIGHLIGHT_UP;
 
             case KEY_POWER:
@@ -75,25 +73,15 @@ int device_handle_key(int key_code, int visible) {
                 if (!get_allow_toggle_display())
                     return GO_BACK;
                 break;
-            case KEY_LEFTBRACE:
-            case KEY_ENTER:
-            case BTN_MOUSE:
-            case KEY_CENTER:
-            case KEY_CAMERA:
-            case KEY_F21:
-            case KEY_SEND:
+
+            //case KEY_MENU:
+	    case 158:
                 return SELECT_ITEM;
             
-            case KEY_END:
-            case KEY_BACKSPACE:
-            case KEY_SEARCH:
-                if (ui_get_showing_back_button()) {
-                    return SELECT_ITEM;
-                }
+	    //case KEY_BACK
+	    case 102:
                 if (!get_allow_toggle_display())
                     return GO_BACK;
-            case KEY_BACK:
-                return GO_BACK;
         }
     }
 
