72e14c3f8eab0f7b8bae1f2a31c6bba76b579239
Isaac Joseph <ikarosdev@gmail.com>
2011-08-03 20:28:08 -0500

    let's try this

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -75,5 +75,6 @@ BOARD_HAS_NO_MISC_PARTITION := true
 
 # Recovery
 BOARD_USES_MMCUTILS := true
-BOARD_CUSTOM_RECOVERY_KEYMAPPING:= ../../device/motorola/triumph/recovery/recovery_ui.c
+BOARD_CUSTOM_RECOVERY_KEYMAPPING := ../../device/motorola/triumph/recovery/recovery_ui.c
+BOARD_CUSTOM_GRAPHICS := ../../../device/motorola/triumph/recovery/minui/graphics.c
 BOARD_HAS_NO_SELECT_BUTTON := true
