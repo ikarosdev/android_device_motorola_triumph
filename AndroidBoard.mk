6bbcad81d32cb325c2b4bed4bc1cf1f731d84f98
ikarosdev <ikarosdev@gmail.com>
2011-12-04 12:56:58 -0600

    triumph: update makefile to pick up prebuilt kernel

--- AndroidBoard.mk
+++ AndroidBoard.mk
@@ -2,7 +2,10 @@ LOCAL_PATH := $(call my-dir)
 
 include $(CLEAR_VARS)
 
-ALL_PREBUILT += $(INSTALLED_KERNEL_TARGET)
+file := $(INSTALLED_KERNEL_TARGET)
+ALL_PREBUILT += $(file)
+$(file): $(TARGET_PREBUILT_KERNEL) | $(ACP)
+	$(transform-prebuilt-to-target)
 
 # include the non-open-source counterpart to this file
 -include vendor/motorola/triumph/AndroidBoardVendor.mk
