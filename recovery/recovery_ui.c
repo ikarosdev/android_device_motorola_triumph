790f65d2ac311c7c6172a665725a853e131f4c89
ikarosdev <ikarosdev@gmail.com>
2011-12-04 12:31:24 -0600

    Update custom recovery keymapping for ICS, point makefile to renamed file

--- recovery/recovery_ui.c
+++ recovery/recovery_ui.c
@@ -20,25 +20,6 @@
 #include "common.h"
 #include "extendedcommands.h"
 
-char* MENU_HEADERS[] = { "Use vol keys to highlight and menu to select.",
-                         "",
-                         NULL };
-
-char* MENU_ITEMS[] = { "reboot system now",
-                       "apply update from sdcard",
-                       "wipe data/factory reset",
-                       "wipe cache partition",
-                       "install zip from sdcard",
-                       "backup and restore",
-                       "mounts and storage",
-                       "advanced",
-                       "power off",
-                       NULL };
-
-int device_recovery_start() {
-    return 0;
-}
-
 int device_toggle_display(volatile char* key_pressed, int key_code) {
     int alt = key_pressed[KEY_LEFTALT] || key_pressed[KEY_RIGHTALT];
     if (alt && key_code == KEY_L)
@@ -49,10 +30,6 @@ int device_toggle_display(volatile char* key_pressed, int key_code) {
         //return get_allow_toggle_display() && (key_code == KEY_HOME || key_code == KEY_MENU || key_code == KEY_END);
     }
     return get_allow_toggle_display() && (key_code == KEY_POWER || key_code == KEY_END);
-}
-
-int device_reboot_now(volatile char* key_pressed, int key_code) {
-    return 0;
 }
 
 int device_handle_key(int key_code, int visible) {
@@ -73,12 +50,4 @@ int device_handle_key(int key_code, int visible) {
     }
 
     return NO_ACTION;
-}
-
-int device_perform_action(int which) {
-    return which;
-}
-
-int device_wipe_data() {
-    return 0;
 }
