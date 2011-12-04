283809dbe1afb27363afdb594d835d84bb86f6ca
ikarosdev <ikarosdev@gmail.com>
2011-12-04 13:07:13 -0600

    triumph: update CM makefile

--- cm.mk
+++ cm.mk
@@ -1,3 +1,19 @@
+#
+# Copyright (C) 2011 The Android Open-Source Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
 $(call inherit-product, device/motorola/triumph/device_triumph.mk)
 
 # Inherit some common CM stuff.
@@ -6,8 +22,24 @@ $(call inherit-product, vendor/cm/config/common_full_phone.mk)
 # Inherit some common CM stuff.
 #$(call inherit-product, vendor/cm/config/gsm.mk)
 
-PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=soju BUILD_ID=GWK74 BUILD_FINGERPRINT=google/soju/crespo:2.3.7/GWK74/185293:user/release-keys PRIVATE_BUILD_DESC="soju-user 2.3.7 GWK74 185293 release-keys" BUILD_NUMBER=185293
-
 PRODUCT_NAME := cm_triumph
+PRODUCT_BRAND := Motorola
 PRODUCT_DEVICE := triumph
+PRODUCT_MODEL := WX435
+PRODUCT_MANUFACTURER := Motorola
 
+# Release name and versioning
+PRODUCT_RELEASE_NAME := Triumph
+
+UTC_DATE := $(shell date +%s)
+DATE     := $(shell date +%Y%m%d)
+
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    BUILD_ID=GWK74 \
+    PRODUCT_NAME=triumph \
+    TARGET_DEVICE=triumph \
+    BUILD_FINGERPRINT=google/soju/crespo:2.3.7/GWK74/185293:user/release-keys \
+    PRIVATE_BUILD_DESC="soju-user 2.3.7 GWK74 185293 release-keys" \
+    BUILD_NUMBER=${DATE} \
+    BUILD_VERSION_TAGS=release-keys \
+    TARGET_BUILD_TYPE=user
