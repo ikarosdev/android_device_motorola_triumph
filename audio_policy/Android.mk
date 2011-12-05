ca82d1078082c824183df3c1fb8771df623dd720
ikarosdev <ikarosdev@gmail.com>
2011-12-05 15:16:41 -0600

    triumph: helps if you commit the audio changes, push updated EGL libs

---
+++ audio_policy/Android.mk
@@ -0,0 +1,44 @@
+ifeq ($(TARGET_BOOTLOADER_BOARD_NAME),triumph)
+
+LOCAL_PATH:= $(call my-dir)
+
+include $(CLEAR_VARS)
+
+LOCAL_SRC_FILES:= AudioPolicyManager.cpp
+LOCAL_SHARED_LIBRARIES:= libc libcutils libutils libmedia
+LOCAL_STATIC_LIBRARIES := libmedia_helper
+LOCAL_WHOLE_STATIC_LIBRARIES:= libaudiopolicy_legacy
+LOCAL_MODULE_PATH := $(TARGET_OUT_SHARED_LIBRARIES)/hw
+LOCAL_MODULE:= audio_policy.triumph
+LOCAL_MODULE_TAGS := optional
+
+ifeq ($(BOARD_HAVE_BLUETOOTH),true)
+  LOCAL_CFLAGS += -DWITH_A2DP
+endif
+
+include $(BUILD_SHARED_LIBRARY)
+
+
+ifeq ($(BOARD_USES_AUDIO_LEGACY),true)
+
+LOCAL_PATH := $(call my-dir)
+include $(CLEAR_VARS)
+LOCAL_MODULE := audio.primary.triumph
+LOCAL_MODULE_PATH := $(TARGET_OUT_SHARED_LIBRARIES)/hw
+LOCAL_MODULE_TAGS := optional
+LOCAL_SHARED_LIBRARIES := \
+    libcutils \
+    libutils \
+    libmedia \
+    libhardware
+LOCAL_SHARED_LIBRARIES += libdl
+LOCAL_SHARED_LIBRARIES += libaudio
+LOCAL_STATIC_LIBRARIES := \
+    libmedia_helper
+LOCAL_WHOLE_STATIC_LIBRARIES := \
+    libaudiohw_legacy
+include $(BUILD_SHARED_LIBRARY)
+
+endif
+endif #BOARD_USES_AUDIO_LEGACY
+
